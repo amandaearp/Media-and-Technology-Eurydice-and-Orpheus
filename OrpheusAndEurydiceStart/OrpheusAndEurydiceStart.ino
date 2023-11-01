261ae1eda776edeb96e58bdc1b34245fda30f765
amandaearp <149627384+amandaearp@users.noreply.github.com>
2023-11-01 17:03:42 -0500

    Servo and Switch Working OrpheusAndEurydiceStart.ino

--- OrpheusAndEurydiceStart/OrpheusAndEurydiceStart.ino
+++ OrpheusAndEurydiceStart/OrpheusAndEurydiceStart.ino
@@ -1,21 +1,22 @@
+#include <Servo.h>
+Servo myservo;
 const int buttonPin = 8;
-const int LED = 11;
 int buttonVal;
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(buttonPin, INPUT);
-pinMode(LED, OUTPUT);
+myservo.attach(9);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 buttonVal = digitalRead(buttonPin);
 
-  if(buttonVal == LOW) {
-    digitalWrite(LED, HIGH);
+  if(buttonVal == HIGH) {
+    myservo.write(180);
 }
   else{
-     digitalWrite(LED, LOW);
+    myservo.write(0);
 }
 }
