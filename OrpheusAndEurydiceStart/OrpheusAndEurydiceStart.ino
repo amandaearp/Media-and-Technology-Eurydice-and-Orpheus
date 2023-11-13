58b32def65e93d6dec5683bfbd6bc8f212e71523
amandaearp <149627384+amandaearp@users.noreply.github.com>
2023-11-13 15:57:07 -0600

    Final Code Prototype OrpheusAndEurydiceStart.ino
    Prototype for code used for full playtesting

--- OrpheusAndEurydiceStart/OrpheusAndEurydiceStart.ino
+++ OrpheusAndEurydiceStart/OrpheusAndEurydiceStart.ino
@@ -4,17 +4,17 @@ Project: The Myth of Orpheus and Eurydice
   */
 #include <Servo.h>
 Servo OverworldEurydice;
-const int OrpheusOriginalSpot = 7;
-const int EurydiceRecieveFlower = 8;
+const int OrpheusOriginalSpot = 5;
+const int EurydiceRecieveFlower = 7;
 int OrpheusOriginalState;
 int OrpheusOriginalPreviousState;
 int EurydiceFlowerState;
 
-const int OrpheusArriveUnderworld = 6;
+const int OrpheusArriveUnderworld = 3;
 int OrpheusArriveUnderworldState;
 Servo EurydiceAliveUnderworld;
 Servo Hades;
-const int OrpheusLeaveUnderworld = 5;
+const int OrpheusLeaveUnderworld = 2;
 int OrpheusLeaveUnderworldState;
 Servo EurydiceDeadUnderworld;
 
@@ -28,6 +28,12 @@ OverworldEurydice.attach(10);
 EurydiceAliveUnderworld.attach(11);
 Hades.attach(12);
 EurydiceDeadUnderworld.attach(13);
+//Reset servos
+  OverworldEurydice.write(0);
+  EurydiceAliveUnderworld.write(0);
+  Hades.write(0);
+  EurydiceAliveUnderworld.write(0);
+  EurydiceDeadUnderworld.write(0);
 }
 
 void loop() {
@@ -37,9 +43,8 @@ EurydiceFlowerState = digitalRead(EurydiceRecieveFlower);
 
 // Triggers Eurydice to die once Orpheus moves from his original spot AND he gives Eurydice a flower
 if(OrpheusOriginalState != OrpheusOriginalPreviousState){
-  if(OrpheusOriginalState == HIGH && EurydiceFlowerState == HIGH){
-    OverworldEurydice.write(90);
-   EurydiceAliveUnderworld.write(180); //Eurydice UW pops up????
+  if(OrpheusOriginalState == LOW && EurydiceFlowerState == HIGH){
+  OverworldEurydice.write(180);
   }
 
 }
@@ -50,19 +55,13 @@ OrpheusArriveUnderworldState = digitalRead(OrpheusArriveUnderworld);
 OrpheusLeaveUnderworldState = digitalRead(OrpheusLeaveUnderworld);
 
 if(OrpheusArriveUnderworldState == HIGH){
- Hades.write(180);
-}
-else{
-  Hades.write(0);
+ OverworldEurydice.write(90);
+ Hades.write(90);
+ EurydiceAliveUnderworld.write(90); //Eurydice UW pops up????
 }
 
 if(OrpheusLeaveUnderworldState == HIGH){
  EurydiceAliveUnderworld.write(0);
- EurydiceDeadUnderworld.write(180);
+ EurydiceDeadUnderworld.write(90);
 }
-else{
-  EurydiceAliveUnderworld.write(0);
-  EurydiceDeadUnderworld.write(180);
-}
-
 }
