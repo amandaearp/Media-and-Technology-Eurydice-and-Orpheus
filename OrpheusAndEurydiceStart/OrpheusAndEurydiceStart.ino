5adb424fb32c88af9369342b91c874138700b06d
amandaearp <149627384+amandaearp@users.noreply.github.com>
2023-11-24 23:25:39 -0600

    Orpheus and Eurydice LED prototype code
    Code that expands upon the prototype to add LEDs, creating new affordances for the user.

--- OrpheusAndEurydiceStart/OrpheusAndEurydiceStart.ino
+++ OrpheusAndEurydiceStart/OrpheusAndEurydiceStart.ino
@@ -3,13 +3,16 @@ Team: Amanda Earp and Rachel Wright
 Project: The Myth of Orpheus and Eurydice
 */
 #include <Servo.h>
+const int SunLED = 8;
 Servo OverworldEurydice;
-const int OrpheusOriginalSpot = 5;
+const int OrpheusOriginalSpot = 6;
 const int EurydiceRecieveFlower = 7;
 int OrpheusOriginalState;
 int OrpheusOriginalPreviousState;
 int EurydiceFlowerState;
 
+const int FireLED1 = 5;
+const int FireLED2 = 4;
 const int OrpheusArriveUnderworld = 3;
 int OrpheusArriveUnderworldState;
 Servo EurydiceAliveUnderworld;
@@ -22,6 +25,9 @@ Servo EurydiceDeadUnderworld;
 void setup() {
 
 pinMode(OrpheusOriginalSpot, INPUT);
+pinMode (SunLED, OUTPUT);
+pinMode (FireLED1, OUTPUT);
+pinMode (FireLED2, OUTPUT);
 Serial.begin(9600);
 pinMode(EurydiceRecieveFlower, INPUT);
 OverworldEurydice.attach(10);
@@ -29,7 +35,10 @@ EurydiceAliveUnderworld.attach(12);
 EurydiceDeadUnderworld.attach(11);
 Hades.attach(13);
 
-//Reset servos
+//Reset servos and LEDS
+  digitalWrite(SunLED, HIGH);
+  digitalWrite(FireLED1, LOW);
+  digitalWrite(FireLED2, LOW);
   OverworldEurydice.write(180);
   EurydiceAliveUnderworld.write(0);
   Hades.write(0);
@@ -44,8 +53,11 @@ EurydiceFlowerState = digitalRead(EurydiceRecieveFlower);
 
 // Triggers Eurydice to die once Orpheus moves from his original spot AND he gives Eurydice a flower
 if(OrpheusOriginalState != OrpheusOriginalPreviousState){
-  if(OrpheusOriginalState == HIGH){
+  if(OrpheusOriginalState == HIGH && EurydiceRecieveFlower == HIGH){
   OverworldEurydice.write(0);
+  digitalWrite(SunLED, LOW);
+  digitalWrite(FireLED1, HIGH);
+  digitalWrite(FireLED2, HIGH);
   }
 
 }
